33100da30fc6778f4ec848ee7ee652b97b322afe
Meera Kofkin <meerakofkin05@gmail.com>
2025-10-02 11:51:02 +0100

    exercise 1-2 complete

---
+++ exercise1-2 copy/sketch.js
@@ -0,0 +1,17 @@
+let x = 0;
+let moveAmount;
+
+function setup() {
+    createCanvas(600, 600);
+    moveAmount = random(1,10);
+}
+
+function draw() {
+    
+    background(0);
+    circle(x, height / 2, 100);
+    if (x < 0 || x > width) {
+        moveAmount *= -1
+    }
+    x += moveAmount;
+}
